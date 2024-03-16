b8a1c99e621d9ad7ebdda5771593736ff427c7f7
faruk <77586873+faruktanriverdi@users.noreply.github.com>
2024-03-17 00:00:34 +0300

    Add files via upload

---
+++ r-2/5.ders/5.ders.R
@@ -0,0 +1,57 @@
+
+#|  Box Plot  
+
+#| Necessary Libraries
+
+#install.packages("tidyverse")
+#install.packages("hrbrthemes")
+#install.packages("ggpubr")
+#install.packages("gfonts")
+
+library(readxl)  #excel dosyasi okutmak icin
+library(tidyverse) #pivotlonger için
+library(dplyr)  # %>% isaretini aktiflestirmek icin
+library(hrbrthemes) # theme_ipsum() fonksiyonu icin
+library(ggpubr) #ggviolin ve ggboxplot fonksiyonlari icin
+library(gfonts)
+
+# Data Import
+
+setwd("C:\\Users\\faruk\\Desktop\\r-2")
+data<-read_xlsx("data_source.xlsx",sheet = 5)
+
+# Data Toparlama / Duzenleme 
+colnames(data)
+data<-data %>% 
+  pivot_longer(
+    cols = c("A_Spor","B_Spor","C_Spor","D_Spor","E_Spor"),
+    names_to = "kanal",
+    values_to = "reyting"
+    )
+
+# Tek Renkli Box Plot, Legend Olmadan
+
+ggplot(data,aes(x = kanal,y = reyting,group=kanal))+
+  geom_boxplot(fill="lightblue",notch = FALSE, outlier.shape = NA)+
+  geom_jitter(size=1,color="black",width = 0.1)+
+  theme_ipsum()
+
+## Ozel Renkli Box Plot 
+
+ggboxplot(data, x = "kanal", y = "reyting", fill = "kanal",
+          palette = c("red","blue", "orange", "lightblue","purple"),
+          add = "boxplot", add.params = list(fill = rep(c("red","blue", "orange", "lightblue","purple"))))
+
+## Ozel Renkli Violin Plot 
+
+ggviolin(data, x = "kanal", y = "reyting", fill = "kanal",
+         palette = c("red","blue", "orange", "lightblue","purple"),
+         add = "boxplot", add.params = list(fill = "white"))+
+  theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust = 1))
+
+
+
+
+
+
+
